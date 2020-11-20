a87d4aab79a93abfec608880c4a8e754c1434afa
Richard Z <zhang.richard.rz@gmail.com>
2020-11-19 22:05:04 -0500

    downscale app resolution for performance

--- pinest/gui.py
+++ pinest/gui.py
@@ -1,4 +1,5 @@
 # Target display: 1024x600 touchscreen, raspberry pi 3
+# Target resolution: mode 640x480
 import os, time
 import tkinter as tk
 
@@ -12,7 +13,7 @@ class CurrentWeatherGUI():
         now = time.localtime()
         now_hr = now[3] # https://docs.python.org/3/library/time.html#time.struct_time
         base_path = os.path.dirname(__file__)
-        bg_path = 'assets\\bg_night.png' if now_hr < 7 or now_hr > 19 else 'assets\\bg_day.png'
+        bg_path = 'assets\\bg_night.png' if now_hr < 7 or now_hr > 19 else 'assets\\bg_day.png' # TODO: / for linux and \ for windows
         path = os.path.join(base_path, bg_path)
         bg = tk.PhotoImage(file=path)
         bg_label = tk.Label(root, image=bg)
